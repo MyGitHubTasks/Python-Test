1537d31bf559626c359c86f13d4c7a11c921b354
SomannaKuri <somannalkuri@gmail.com>
2019-09-27 21:47:30 +0530

    First commit

---
+++ simpleSort.py
@@ -0,0 +1,9 @@
+data = [64, 25, 12, 22, 11,-1, 1,2,44,3,122, 23, 34]
+
+for i in range(len(data)):
+    for j in range(i + 1, len(data)):
+
+        if data[i] > data[j]:
+           data[i], data[j] = data[j], data[i]
+
+print (data)
